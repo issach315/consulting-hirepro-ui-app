6eff7c5066342bf7cb07438f27e62e1c5a304bfc
issach315 <issachemani227@gmail.com>
2026-02-19 12:14:42 +0530

    url changes in appclient

--- src/service/appClient.js
+++ src/service/appClient.js
@@ -3,8 +3,7 @@ import axios from "axios";
 /* ============================
    BASE URL (Vite)
 ============================ */
-const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+const BASE_URL = "https://consulting-hirepro-service-development.up.railway.app/api";
 
 /* ============================
    AXIOS INSTANCE (Cookie Based)
